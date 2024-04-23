c70f3e15a3a7a3d69a1d8f3ac8b5eb9eaf9021ff
Ashton-Stockenberg <as.j.stockenberg@baypath.net>
2024-04-23 12:31:58 -0400

    HOW COULD U... THEY WERE INNOCENT

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const front = document.querySelector('#name-front')
-const back = document.querySelector('#name-back')
+// Call to action
+{
+    const start_time = Date.now()
+    let sin_result = 0
 
-document.addEventListener('mousemove', (data) => {
-    const centerX = window.innerWidth/2
-    const centerY = window.innerHeight/2
+    const front = document.querySelector('#name-front')
+    const back = document.querySelector('#name-back')
+    const logo = document.querySelector('#github-logo')
+    
+    let deltaX = 0
+    let deltaY = 0
 
-    const deltaX = centerX - data.clientX
-    const deltaY = centerY - data.clientY
+    document.addEventListener('mousemove', (data) => {
+        const centerX = window.innerWidth/2
+        const centerY = window.innerHeight/2
 
-    back.style.left = `${deltaX/60}px`
-    back.style.top = `${deltaY/60}px`
+        deltaX = (centerX - data.clientX)/60
+        deltaY = (centerY - data.clientY)/60
 
-    front.style.left = `${-deltaX/60}px`
-    front.style.top = `${-deltaY/60}px`
-})
+        back.style.left = `${deltaX}px`
+        back.style.top = `${deltaY + sin_result}px`
+
+        front.style.left = `${-deltaX}px`
+        front.style.top = `${-deltaY + sin_result}px`
+    })
+
+    setInterval(() => {
+        const running_time = Date.now() - start_time
+        sin_result = Math.sin((running_time/500))*15
+
+        back.style.top = `${deltaY + sin_result}px`
+        front.style.top = `${-deltaY + sin_result}px`
+        
+    }, 1)
+}
